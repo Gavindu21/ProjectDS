81bdff6f878a6460511cab0f57f4449c4a12ee16
Gavindu <gavindu.sg@gmail.com>
2025-03-22 10:58:11 +0530

    complete

--- ProjectDS.py
+++ ProjectDS.py
@@ -1,5 +1,19 @@
-# simple_data_analysis.py
-print("ProjectDS Data Science project!")
-numbers = [1, 2, 3, 4, 5]
-print("Sum of numbers:", sum(numbers))
-print("Average of numbers:", sum(numbers) / len(numbers))
+
+# iris_analysis.py
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+iris = pd.read_csv(url)
+
+print("Dataset Info:")
+print(iris.head())
+print(iris.describe())
+
+
+sns.scatterplot(x="sepal_length", y="sepal_width", hue="species", data=iris)
+plt.title("Sepal Length vs Sepal Width by Species")
+plt.savefig("iris_plot.png")  
+plt.show()
